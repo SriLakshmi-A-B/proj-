f96093ebc3d210e44af1a9527069e11aae87416f
SriLakshmi-A-B <24suit47@tcarts.in>
2025-11-19 17:21:56 +0530

    Add files via upload

---
+++ NEW1/script/script.js
@@ -0,0 +1,3 @@
+ document.getElementById("actionBtn").addEventListener("click", () => {
+    document.getElementById("output").textContent = "You clicked the button! Copilot helped build this!";
+});
